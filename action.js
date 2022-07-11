234104b2d19e4b79ec3ea7159f9c727860946fde
cvkshade <cvkshade@gmail.com>
2022-07-11 15:32:02 +0000

    Feel free to do your own thing

---
+++ action.js
@@ -0,0 +1,19 @@
+// Card Collapse
+const infoCardHeaders = document.querySelectorAll('.info_card_header');
+
+infoCardHeaders.forEach((infoCardHeader) => {
+  infoCardHeader.addEventListener('click', (event) => {
+    const activeBars = document.querySelector('.info_card_header.active');
+    if (activeBars && activeBars !== infoCardHeader) {
+      activeBars.classList.remove('active');
+      activeBars.nextElementSibling.style.maxHeight = 0;
+    }
+    infoCardHeader.classList.toggle('active');
+    const cardBodyMain = infoCardHeader.nextElementSibling;
+    if (infoCardHeader.classList.contains('active')) {
+      cardBodyMain.style.maxHeight = cardBodyMain.scrollHeight + 'px';
+    } else {
+      cardBodyMain.style.maxHeight = 0;
+    }
+  });
+});
